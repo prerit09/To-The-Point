72f71152902dc3b853c2d5844a26e43b3c3229c1
prerit09 <preritj.27@gmail.com>
2020-12-06 16:13:01 +0530

    variables tutorial

---
+++ Python/2_variables.py
@@ -0,0 +1,96 @@
+integer_var = 1234
+
+# print(integer_var)
+
+float_var = 100.50
+
+# print(float_var) (Comment - Ctrl + /)
+
+string_var = "Prerit"
+
+# print(string_var)
+
+# print(string_var[0])
+
+# print(string_var[2:5])
+
+# print(string_var[3:])
+
+# print(string_var * 5)
+
+# print(string_var + " Jain")
+
+list_var = ["string_item", 12345, 25.50, "string_2", 9876]
+
+list_var[1] = 11111
+
+# print(list_var)
+
+# print(list_var)
+
+# print(list_var[2])
+
+# print(list_var[1:])
+
+# print(list_var[2:4])
+
+# print(list_var[-1])
+
+# print(list_var[-3])
+
+tuple_var = ("Prerit", 1234, "Jain", 25.5)
+
+# print(tuple_var)
+
+# print(tuple_var[1])
+
+dictionary_var = {
+    "Name" : 'Prerit',
+    "Age" : 25,
+    "Work" : "Engineer"
+}
+
+# print(dictionary_var)
+
+# print(dictionary_var.keys())
+
+# print(dictionary_var.values())
+
+# print(dictionary_var["Age"])
+
+'''Multiple line comment
+Continued comment
+
+Till here'''
+
+word = "WORD"
+sentence = "This is a full sentence."
+paragraph = """Paragraph continues from here and goes all the way to 
+the next line and it stops here."""
+
+# print(word)
+
+# print(sentence)
+
+# print(paragraph)
+
+num1 = 5
+num2 = 10
+num3 = 20
+
+# Multi-line statements
+sum = num1 + \
+    num2 + \
+    num3
+
+# print(sum)
+
+# print(num1 + num2 + num3)
+
+boolean_var1 = True
+
+boolean_var2 = False
+
+# print(boolean_var1)
+
+# print(boolean_var2)
